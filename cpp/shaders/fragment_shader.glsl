cfb75a91fc9a43b3b1abd7435797d98bc5c990fc
Fernando Diaz Smith <fdiazsmith@gmail.com>
2024-06-14 16:56:33 -0700

    flip the shader uv

--- cpp/shaders/fragment_shader.glsl
+++ cpp/shaders/fragment_shader.glsl
@@ -4,7 +4,9 @@ uniform sampler2D tex1;
 uniform sampler2D tex2;
 uniform float alpha;
 void main() {
-    vec4 color1 = texture2D(tex1, v_texcoord);
-    vec4 color2 = texture2D(tex2, v_texcoord);
+    vec2 uv = vec2( 1.0 - v_texcoord.x, 1.0 - v_texcoord.y);
+    uv.x = 1.0 - uv.x;
+    vec4 color1 = texture2D(tex1, uv);
+    vec4 color2 = texture2D(tex2, uv);
     gl_FragColor = mix(color1, color2, alpha);
 }
