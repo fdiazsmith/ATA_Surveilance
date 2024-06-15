6d444fe674c8a4d0b98fb382118031ba1a588c24
Fernando Diaz Smith <fdiazsmith@gmail.com>
2024-06-14 18:14:32 -0700

    delete compile before build

--- cpp/compile.sh
+++ cpp/compile.sh
@@ -2,6 +2,8 @@
 
 # Create build directory if it doesn't exist
 mkdir -p build
+# delete the executable if it exists
+rm build/gstreamer_opengl
 
 # Compile the project
 g++ -o build/gstreamer_opengl src/main.cpp src/opengl_utils.cpp src/gstreamer_utils.cpp `pkg-config --cflags --libs glew glfw3 gstreamer-1.0 gstreamer-app-1.0` -lGLESv2
